3fc776ec52c3a64031dd2b6de193fdadeabd7f49
のぶ <nobuhiko.kimoto@gmail.com>
2021-08-31 16:34:03 +0900

    webview

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,101 @@
+import 'dart:async';
+//import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+    MaterialApp(debugShowCheckedModeBanner: false, home: WebViewExample()));
 
-class MyApp extends StatelessWidget {
+class EmptyAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: '世界のウミウシ',
-        home: Scaffold(
-            appBar: AppBar(
-                title: const Text('SEASLUG.WORLD'),
-            ),
-            body: WebView(
-                initialUrl: 'https://seaslug.world/',
-                javascriptMode: JavascriptMode.unrestricted
+    return Container(
+      color: Colors.white,
+    );
+  }
 
-            ),
-            bottomNavigationBar: BottomNavigationBar(
-                items: const [
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.home),
-                      title: Text('Home'),
-                  ),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.home),
-                      title: Text('Home'),
-                  ),
-                ],
-            ),
-          ),
+  @override
+  Size get preferredSize => Size(0.0, 0.0);
+}
+
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: EmptyAppBar(),
+      // We're using a Builder here so we have a context that is below the Scaffold
+      // to allow calling Scaffold.of(context) so we can show a snackbar.
+      body: Builder(builder: (BuildContext context) {
+        return WebView(
+          initialUrl: "https://seaslug.world",
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+          onProgress: (int progress) {
+            print("WebView is loading (progress : $progress%)");
+          },
+          javascriptChannels: <JavascriptChannel>{
+            _toasterJavascriptChannel(context),
+          },
+          onPageStarted: (String url) {
+            print('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            print('Page finished loading: $url');
+          },
+          gestureNavigationEnabled: true,
         );
+      }),
+      /*bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+            print(index); // デバッグ用にprintしてみる
+          });
+        },
+        //currentIndex: _currentIndex,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+      ),*/
+    );
+  }
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+        name: 'Toaster',
+        onMessageReceived: (JavascriptMessage message) {
+          // ignore: deprecated_member_use
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        });
   }
 }
